aa58f4d5c21af863b87ba238f51a153505a283e9
AleksandrEgorov <ferociousrat@mail.ru>
2022-09-06 18:11:05 +0300

    dz smarthome

--- measurement/views.py
+++ measurement/views.py
@@ -37,65 +37,13 @@ class SensorView(RetrieveAPIView):
 
 
 class MeasurementsView(APIView):
-
-    # def get_pk(self, pk):
-    #     return Sensor.objects.get(pk=pk)
-
     def get(self, request):
         meas = Measurement.objects.all()
         ser = MeasurementSerializer(meas, many=True)
         return Response(ser.data)
 
     def post(self, request):
-        req = request.data
-        meas = Measurement.objects.all()
-        ser = AdvSensorsSerializer(meas, data=req)
-        sens = Sensor.objects.filter(id=req['sensor'])
-        ser.is_valid()
-        ser.save()
-
+        sens = Sensor.objects.get(id=request.data['sensor'])
+        meas = Measurement.objects.create(sensor=sens, temperature=request.data['temperature'])
+        ser = MeasurementSerializer(meas, data=request.data)
         return Response({'XPEHb': 'XPEHb'})
-
-    # def post(self, request):
-    #     meas = Measurement.objects.all()
-    #     ser = AdvSensorsSerializer(meas, data=request.data, many=True)
-    #     print(request.data)
-    #     if ser.is_valid():
-    #         ser.save()
-    #     return Response({'XPEHb': 'XPEHb'})
-    #
-
-    # def post(self, request):
-        # gpk = self.get_pk(request.data['sensor'])
-        # sens = Sensor.objects.get(pk=gpk)
-        # sens = Sensor.objects.get(id=request.data['sensor'])
-
-
-        # print(sens)
-        # meas_ser = AdvSensorsSerializer(data=request.data)
-        # print(meas_ser)
-        # meas = Measurement.objects.all()
-        # meas_ser = MeasurementSerializer(meas, data=request.data, many=True)
-        # meas_advser = AdvSensorsSerializer(meas, data=request.data, many=True)
-        #
-        # if meas.is_valid():
-        #     meas.save()
-        #     return Response({'XPEHb': '___'})
-        #
-
-        # if meas_ser.is_valid():
-        #     meas_ser.save()
-        #     return Response({'XPEHb': '___'})
-        #
-        # sens = Sensor.objects.filter(id=request.data['sensor'])
-        # sens2 = Sensor.objects.get(id=request.data['sensor'])
-        # sens3 = request.data.get('sensor')
-        # sens_ser = SensorsSerializer(sens, data=request.data, many=True, partial=True)
-        # print(sens3)
-        # print(sens_ser)
-        #
-        # if sens_ser.is_valid():
-        #     sens_ser.save()
-        #     return Response({'XPEHb': '___'})
-        #
-        # return Response({'XPEHb': 'XPEHb'})
